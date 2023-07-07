c8944e7bc96322fa4d68b102fe91afb077a44329
hiromu-kon <hiro.0405soccer@icloud.com>
2023-07-07 09:10:50 +0900

    feat: create hokkaido cities master

---
+++ lib/src/models/city.dart
@@ -0,0 +1,15 @@
+class City {
+  int code;
+  String name;
+  String en;
+  String hira;
+  String kana;
+
+  City({
+    required this.code,
+    required this.name,
+    required this.en,
+    required this.hira,
+    required this.kana,
+  });
+}
